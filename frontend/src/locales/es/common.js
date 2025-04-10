cfacfd820d4604163e554ec9ccda48fdf8c27eeb
Jorge Grijalva <87045579+JorgeGrijalva@users.noreply.github.com>
2025-04-10 12:45:31 -0600

    Update references from AnythingLLM to iAN across the application

--- frontend/src/locales/es/common.js
+++ frontend/src/locales/es/common.js
@@ -116,7 +116,7 @@ const TRANSLATIONS = {
   },
   welcomeMessage: {
     part1:
-      "Bienvenido a AnythingLLM, una herramienta de inteligencia artificial de código abierto creada por Mintplex Labs que convierte cualquier cosa en un chatbot entrenado con el que puedes consultar y conversar. AnythingLLM es un software BYOK (bring-your-own-keys), por lo que no hay suscripciones, tarifas ni cargos por este software, salvo por los servicios que deseas utilizar.",
+      "Bienvenido a IAN, una herramienta de inteligencia artificial de código abierto creada por Mintplex Labs que convierte cualquier cosa en un chatbot entrenado con el que puedes consultar y conversar. AnythingLLM es un software BYOK (bring-your-own-keys), por lo que no hay suscripciones, tarifas ni cargos por este software, salvo por los servicios que deseas utilizar.",
     part2:
       "AnythingLLM es la forma más sencilla de integrar productos de inteligencia artificial potentes como OpenAi, GPT-4, LangChain, PineconeDB, ChromaDB y otros servicios en un paquete ordenado, sin complicaciones, para aumentar tu productividad en un 100x.",
     part3:
